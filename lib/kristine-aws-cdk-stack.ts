02be64311ede0a17ed3f6652482fffa409c9ba4e
kristine-lin <kristine_lin@icloud.com>
2024-07-17 22:07:12 +0800

    CDK S3

--- lib/kristine-aws-cdk-stack.ts
+++ lib/kristine-aws-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class KristineAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +10,19 @@ export class KristineAwsCdkStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'KristineAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an S3 bucket
+   new s3.Bucket(this, 'MyCdkBucket', {
+    // Enable versioning for the bucket
+    versioned: true,
+    // Define the removal policy for the bucket
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    // Automatically delete objects when the bucket is deleted
+    autoDeleteObjects: true,
+  });
+    //const queue = new sqs.Queue(this, 'KristineAwsCdkQueue', {
+    //  visibilityTimeout: cdk.Duration.seconds(300)
+    //});
+
+
   }
 }
